323dcc6debdf57e6291b123ac16ae418ca048984
CastCast <lamanhcuong.hus@gmail.com>
2025-02-28 18:59:11 +0700

    update icon

---
+++ lib/test_page.dart
@@ -0,0 +1,28 @@
+//
+// import 'package:flutter/material.dart';
+//
+// class TestPage extends StatelessWidget {
+//   const TestPage({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Test Page'),
+//       ),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Image(
+//             width: 42,
+//             height: 42,
+//             fit: BoxFit.cover,
+//             image: DefaultImage().eating,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+//
+
